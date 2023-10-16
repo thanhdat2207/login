7fd1a254143783325eb404b20008b9b54f06356f
thanhdat2207 <145658449+thanhdat2207@users.noreply.github.com>
2023-10-16 07:12:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ signupLoginLink.forEach(link => {
         formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
     });
 });
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Ngăn chặn gửi form đi
+
+  // Lấy giá trị từ các input
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // Xử lý dữ liệu đăng nhập theo ý muốn
+  // Ví dụ: Kiểm tra thông tin đăng nhập hợp lệ
+  if (username === 'thanhdat2207' && password === '123456') {
+    // Nếu thông tin hợp lệ, chuyển đến trang thành công
+    window.location.href = "cvcanhan_ThaiThanhDat.html";
+  } else {
+    // Nếu thông tin không hợp lệ, hiển thị thông báo lỗi
+    alert("Tên đăng nhập hoặc mật khẩu không chính xác!");
+  }
+});
+                                        
+                             
